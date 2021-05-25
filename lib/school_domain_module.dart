3e7a8cfa95374793786b1f3ca58ae51202571357
Muhammad Noerhidayatullah <mnoer@outlook.com>
2021-05-25 09:27:40 +0700

    export injection

--- lib/school_domain_module.dart
+++ lib/school_domain_module.dart
@@ -3,3 +3,4 @@ library school_domain_module;
 export 'package:school_domain_module/src/entity/entity.dart';
 export 'package:school_domain_module/src/repository/repository.dart';
 export 'package:school_domain_module/src/usecase/usecase.dart';
+export './src/di/injection_container.dart';
